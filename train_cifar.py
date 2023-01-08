371cd800e11fc2af17c2e7b4fbc75522834a569c
Morales97 <danimoralesbrotons@gmail.com>
2023-01-08 15:54:04 +0100

    Update train_cifar.py

--- train_cifar.py
+++ train_cifar.py
@@ -21,8 +21,7 @@ def evaluate_model(model, data_loader, device):
     correct = 0
     with torch.no_grad():
         for data, target in data_loader:
-            pdb.set_trace()
-            data, target = data.to(device), target.to(device)
+            data = data.to(device)
             output = model(data)
             # sum up batch loss
             loss += F.nll_loss(output, target, reduction='sum').item()
