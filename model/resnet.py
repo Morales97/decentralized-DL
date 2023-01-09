fffc945fdaba6e1b5b4d9a652e4a46c3cd3fe6e6
Morales97 <danimoralesbrotons@gmail.com>
2023-01-09 13:22:44 +0100

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -308,6 +308,6 @@ def resnet18(config):
     return model
 
 if __name__ == '__main__':
-    model = resnet18({'dataset': 'cifar10'})
+    model = resnet18({'dataset': 'cifar10'}).to('cuda')
     summary(model, (3, 32, 32))
     pdb.set_trace()
