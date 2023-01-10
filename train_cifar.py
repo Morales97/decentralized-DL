321b617028b14ab77b59953be13529973997e917
Morales97 <danimoralesbrotons@gmail.com>
2023-01-10 11:49:17 +0100

    Update train_cifar.py

--- train_cifar.py
+++ train_cifar.py
@@ -193,7 +193,7 @@ config = {
     'p_label_skew': 0,
     'net': 'resnet18',
     'wandb': True,
-    'eval_on_average_model': True,
+    'eval_on_average_model': False,
     'dataset': 'cifar10',
 }
 
@@ -203,13 +203,13 @@ config = {
 
 # expt = {'topology': 'solo', 'local_steps': 0}
 # expt = {'topology': 'centralized', 'label': 'Centralized', 'local_steps': 0}
-expt = {'topology': 'fully_connected', 'local_steps': 0}
+# expt = {'topology': 'fully_connected', 'local_steps': 0}
 # expt = {'topology': 'fully_connected', 'local_steps': 5, 'eval_on_average_model': True}
 # expt = {'topology': 'fully_connected', 'local_steps': 16}
 # expt = {'topology': 'fully_connected', 'local_steps': 50}
 # expt = {'topology': 'random', 'degree': 4, 'local_steps': 0}
 # expt = {'topology': 'exponential_graph', 'local_steps': 0}
-# expt = {'topology': 'ring', 'local_steps': 0}
+expt = {'topology': 'ring', 'local_steps': 0}
 # expt = {'topology': 'ring', 'local_steps': 0, 'data_split': 'no', 'eval_on_average_model': True}
 
 if __name__ == '__main__':
