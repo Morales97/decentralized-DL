0a83e3ade1c4c042127b56bbf602a8b39a1ce2b3
Morales97 <danimoralesbrotons@gmail.com>
2023-01-11 19:12:18 +0100

    .

--- experiments/cifar10-vgg-SB.sh
+++ experiments/cifar10-vgg-SB.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=1600 --net=vgg --epochs=100
+python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --net=vgg --epochs=100
