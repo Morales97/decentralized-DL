a47b18d7e63cf2816182492b39d97ce2e35d372d
Morales97 <danimoralesbrotons@gmail.com>
2023-01-18 20:59:28 +0100

    Update cifar100-rn20-solo.sh

--- experiments/cifar100-rn20-solo.sh
+++ experiments/cifar100-rn20-solo.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 # python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=0
-python train_cifar.py --n_nodes=1 --lr=0.4 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=0
-python train_cifar.py --n_nodes=1 --lr=0.4 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=1
-python train_cifar.py --n_nodes=1 --lr=0.4 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=2
+python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=0 &
+python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=1 &
+python train_cifar.py --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --dataset=cifar100 --seed=2 &
+python train_cifar.py --lr=3.2 --topology=ring --dataset=cifar100 --seed=0 &
+python train_cifar.py --lr=3.2 --topology=ring --dataset=cifar100 --seed=1 &
+python train_cifar.py --lr=3.2 --topology=ring --dataset=cifar100 --seed=2 &
+python train_cifar.py --lr=3.2 --topology=fully_connected --eval_on_average_model=True --dataset=cifar100 --seed=0 &
+python train_cifar.py --lr=3.2 --topology=fully_connected --eval_on_average_model=True --dataset=cifar100 --seed=1 &
+python train_cifar.py --lr=3.2 --topology=fully_connected --eval_on_average_model=True --dataset=cifar100 --seed=2 &
+
+
+wait
