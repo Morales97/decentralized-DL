b55bdbf339b627170cd71639ab3d30675e4bc8d6
Morales97 <danimoralesbrotons@gmail.com>
2023-01-25 14:57:07 +0100

    Update train_imagenet.py

--- train_imagenet.py
+++ train_imagenet.py
@@ -399,7 +399,7 @@ def train(train_loader, model, criterion, optimizer, ema_optimizer, epoch, devic
             progress.display(i + 1)
             logger.log_train_IN(step + i, epoch + i/(len(train_loader)), loss.item(), log_time.avg, ts_start)
             log_time = AverageMeter('Time', ':6.3f')
-
+            break
     return i
 
 def validate(val_loader, model, criterion, args, logger, step, epoch, ema=False):
