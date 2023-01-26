4c5a22a9604eb8b691f018ad5f046e5bc88375af
Morales97 <danimoralesbrotons@gmail.com>
2023-01-26 23:36:57 +0100

    Update cifar10-rn20-fraction.sh

--- experiments/cifar10-rn20-fraction.sh
+++ experiments/cifar10-rn20-fraction.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python train_cifar.py --expt_name=solo_baseline_10k --fraction=0.2 --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --seed=0 &
-python train_cifar.py --expt_name=solo_baseline_10k --fraction=0.2 --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --seed=1 &
-python train_cifar.py --expt_name=FC_16_10k --fraction=0.2 --topology=fully_connected --eval_on_average_model=True --lr=3.2 --seed=0 &
-python train_cifar.py --expt_name=FC_16_10k --fraction=0.2 --topology=fully_connected --eval_on_average_model=True --lr=3.2 --seed=1 &
-python train_cifar.py --expt_name=C2.2_10k --fraction=0.2 --topology fully_connected fully_connected fully_connected --local_steps 0 0 0 --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --lr_decay 300 --eval_on_average_model=True --seed=0 &
-python train_cifar.py --expt_name=C2.2_10k --fraction=0.2 --topology fully_connected fully_connected fully_connected --local_steps 0 0 0 --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --lr_decay 300 --eval_on_average_model=True --seed=1 &
+python train_cifar.py --expt_name=solo_baseline_10k --data_fraction=0.2 --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --seed=0 &
+python train_cifar.py --expt_name=solo_baseline_10k --data_fraction=0.2 --n_nodes=1 --lr=0.2 --topology=solo --steps_eval=400 --lr_warmup_epochs=0 --seed=1 &
+python train_cifar.py --expt_name=FC_16_10k --data_fraction=0.2 --topology=fully_connected --eval_on_average_model=True --lr=3.2 --seed=0 &
+python train_cifar.py --expt_name=FC_16_10k --data_fraction=0.2 --topology=fully_connected --eval_on_average_model=True --lr=3.2 --seed=1 &
+python train_cifar.py --expt_name=C2.2_10k --data_fraction=0.2 --topology fully_connected fully_connected fully_connected --local_steps 0 0 0 --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --lr_decay 300 --eval_on_average_model=True --seed=0 &
+python train_cifar.py --expt_name=C2.2_10k --data_fraction=0.2 --topology fully_connected fully_connected fully_connected --local_steps 0 0 0 --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --lr_decay 300 --eval_on_average_model=True --seed=1 &
 wait
