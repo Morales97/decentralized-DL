63c04ad86c84ce3eada89d3cda2ff0d73bda5be9
Morales97 <danimoralesbrotons@gmail.com>
2023-01-29 19:20:46 +0100

    Create cifar10-rn20-config2.7_solo.sh

---
+++ experiments/cifar10-rn20-config2.7_solo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python train_cifar.py --lr=3.2 --expt_name=C2.7_solo --topology solo solo solo --local_steps 0 0 0 --n_nodes 1 1 1 --data_split=True --batch_size 1024 1024 1024 --start_epoch_phases 0 100 200 --epochs=300 --lr 3.2 1.6 0.8 --lr_decay 300 --seed=0 &
+python train_cifar.py --lr=3.2 --expt_name=C2.7_solo --topology solo solo solo --local_steps 0 0 0 --n_nodes 1 1 1 --data_split=True --batch_size 1024 1024 1024 --start_epoch_phases 0 100 200 --epochs=300 --lr 3.2 1.6 0.8 --lr_decay 300 --seed=1 &
+python train_cifar.py --lr=3.2 --expt_name=C2.7_solo --topology solo solo solo --local_steps 0 0 0 --n_nodes 1 1 1 --data_split=True --batch_size 1024 1024 1024 --start_epoch_phases 0 100 200 --epochs=300 --lr 3.2 1.6 0.8 --lr_decay 300 --seed=2 &
+wait
