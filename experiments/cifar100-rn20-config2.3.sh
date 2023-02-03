f7c8343d9063c9ac2c6508a55c505b5c2edd2447
Morales97 <danimoralesbrotons@gmail.com>
2023-02-03 20:25:11 +0100

    Update cifar100-rn20-config2.3.sh

--- experiments/cifar100-rn20-config2.3.sh
+++ experiments/cifar100-rn20-config2.3.sh
@@ -3,9 +3,11 @@
 # python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_node_increase --topology=exponential_graph --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --epochs=300 --lr_decay 300 --dataset=cifar100 --seed=1 &
 # python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_node_increase --topology=exponential_graph --n_nodes 8 16 32 --start_epoch_phases 0 100 200 --epochs=300 --lr_decay 300 --dataset=cifar100 --seed=2 &
 
-python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_8n --topology=exponential_graph --n_nodes=8 --epochs=300 --lr_decay 100 200 --lr_decay_factor=2 --dataset=cifar100 --seed=0 &
-python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_8n --topology=exponential_graph --n_nodes=8 --epochs=300 --lr_decay 100 200 --lr_decay_factor=2 --dataset=cifar100 --seed=1 &
-python train_cifar.py --expt_name=C2.3_EG_mixed --topology exponential_graph exponential_graph fully_connected --n_nodes=8 --epochs=300 --lr_decay=300 --start_epoch_phases 0 100 200 --lr 3.2 1.6 0.8 --dataset=cifar100 --seed=0 &
-python train_cifar.py --expt_name=C2.3_EG_mixed --topology exponential_graph exponential_graph fully_connected --n_nodes=8 --epochs=300 --lr_decay=300 --start_epoch_phases 0 100 200 --lr 3.2 1.6 0.8 --dataset=cifar100 --seed=1 &
+# python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_8n --topology=exponential_graph --n_nodes=8 --epochs=300 --lr_decay 100 200 --lr_decay_factor=2 --dataset=cifar100 --seed=0 &
+# python train_cifar.py --lr=3.2 --expt_name=C2.3_EG_8n --topology=exponential_graph --n_nodes=8 --epochs=300 --lr_decay 100 200 --lr_decay_factor=2 --dataset=cifar100 --seed=1 &
+python train_cifar.py --expt_name=C2.3_EG_mixed_16 --topology=exponential_graph exponential_graph fully_connected --n_nodes=16 --epochs=300 --lr_decay=300 --start_epoch_phases 0 100 200 --lr 3.2 1.6 0.8 --dataset=cifar100 --seed=0 &
+python train_cifar.py --expt_name=C2.3_EG_mixed_16 --topology=exponential_graph exponential_graph fully_connected --n_nodes=16 --epochs=300 --lr_decay=300 --start_epoch_phases 0 100 200 --lr 3.2 1.6 0.8 --dataset=cifar100 --seed=1 &
+python train_cifar.py --expt_name=C2.3_EG_mixed_16 --topology=exponential_graph exponential_graph fully_connected --n_nodes=16 --epochs=300 --lr_decay=300 --start_epoch_phases 0 100 200 --lr 3.2 1.6 0.8 --dataset=cifar100 --seed=2 &
+
 
 wait
