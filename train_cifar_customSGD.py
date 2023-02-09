39bd226cd0692106ef98cca1407e01e5beb6f30b
Morales97 <danimoralesbrotons@gmail.com>
2023-02-09 22:48:26 +0100

    Update train_cifar_customSGD.py

--- train_cifar_customSGD.py
+++ train_cifar_customSGD.py
@@ -409,7 +409,10 @@ def train(args, steps, wandb):
 
         # log consensus distance, weight norm
         if step % args.tracking_interval == 0:
-            compute_model_tracking_metrics(args, logger, models, step, epoch, device, opts=opts)
+            if args.momentum > 0:
+                compute_model_tracking_metrics(args, logger, models, step, epoch, device, opts=opts)
+            else:
+                compute_model_tracking_metrics(args, logger, models, step, epoch, device)
 
         # save checkpoint
         if args.save_model and step % args.save_interval == 0:
