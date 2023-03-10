59f59f022a1f3a6265d7811244b9a5ffab122e58
Morales97 <danimoralesbrotons@gmail.com>
2023-03-10 15:52:36 +0100

    Update cifarX-vgg16-r2.sh

--- experiments/cifarX-vgg16-r2.sh
+++ experiments/cifarX-vgg16-r2.sh
@@ -5,8 +5,9 @@
 python train.py --dataset=cifar$1 --expt_name=SGD_$3 --seed=$2 --lr=$3 --val_fraction=0 --net=vgg16 --wd=5e-4 --avg_index --ema_acc_epoch=$4 --ema_val_epoch=$5 # TODO get epochs automatically from ema ckpts 
 
 # EMA acc
-python train.py --dataset=cifar$1 --expt_name=EMA_acc_$3 --seed=$2 --val_fraction=0 --net=vgg16 --wd=5e-4 --avg_index --lr_decay=constant --epoch_swa=$4 --resume=/mloraw1/danmoral/checkpoints/cifar100/vgg16/SGD_$3_s$2/ema_acc_epoch.pth.tar
+python train.py --dataset=cifar$1 --expt_name=EMA_acc_$3 --seed=$2 --val_fraction=0 --net=vgg16 --wd=5e-4 --avg_index --lr_decay=constant --epoch_swa=$4 --resume=/mloraw1/danmoral/checkpoints/cifar100/vgg16/SGD_$3_s$2/ema_acc_epoch.pth.tar & 
 
 # EMA val
 python train.py --dataset=cifar$1 --expt_name=EMA_val_$3 --seed=$2 --val_fraction=0 --net=vgg16 --wd=5e-4 --avg_index --lr_decay=constant --epoch_swa=$5 --resume=/mloraw1/danmoral/checkpoints/cifar100/vgg16/SGD_$3_s$2/ema_val_epoch.pth.tar
 
+wait
