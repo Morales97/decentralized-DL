9dd3f32bde982e0d7541dd6b65219c0ae236c6bb
Morales97 <danimoralesbrotons@gmail.com>
2023-03-14 17:30:59 +0100

    .

--- experiments/tiny-rn18-r2.sh
+++ experiments/tiny-rn18-r2.sh
@@ -5,8 +5,8 @@
 python train.py --dataset=tiny-in --expt_name=SGD_$2 --seed=$1 --lr=$2 --val_fraction=0 --net=rn18 --avg_index --epochs=150 --ema_acc_epoch=$3 --ema_val_epoch=$4 # TODO get epochs automatically from ema ckpts 
 
 # EMA acc
-python train.py --dataset=tiny-in --expt_name=EMA_acc_$3 --seed=$2 --val_fraction=0 --net=rn18 --avg_index --epochs=150 --lr_decay=constant --epoch_swa=$4 --resume=/mloraw1/danmoral/checkpoints/cifar100/rn18/SGD_$3_s$2/ema_acc_epoch.pth.tar
+python train.py --dataset=tiny-in --expt_name=EMA_acc_$2 --seed=$1 --val_fraction=0 --net=rn18 --avg_index --epochs=150 --lr_decay=constant --epoch_swa=$3 --resume=/mloraw1/danmoral/checkpoints/cifar100/rn18/SGD_$2_s$1/ema_acc_epoch.pth.tar
 
 # EMA val
-python train.py --dataset=tiny-in --expt_name=EMA_val_$3 --seed=$2 --val_fraction=0 --net=rn18 --avg_index --epochs=150 --lr_decay=constant --epoch_swa=$5 --resume=/mloraw1/danmoral/checkpoints/cifar100/rn18/SGD_$3_s$2/ema_val_epoch.pth.tar
+python train.py --dataset=tiny-in --expt_name=EMA_val_$2 --seed=$1 --val_fraction=0 --net=rn18 --avg_index --epochs=150 --lr_decay=constant --epoch_swa=$4 --resume=/mloraw1/danmoral/checkpoints/cifar100/rn18/SGD_$2_s$1/ema_val_epoch.pth.tar
 
