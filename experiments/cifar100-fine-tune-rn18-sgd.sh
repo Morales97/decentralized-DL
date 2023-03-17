e030dd9ce244c3f0a64c08490c799eb547e74896
Morales97 <danimoralesbrotons@gmail.com>
2023-03-17 13:34:43 +0100

    .

--- experiments/cifar100-fine-tune-rn18-sgd.sh
+++ experiments/cifar100-fine-tune-rn18-sgd.sh
@@ -3,5 +3,5 @@
 python train.py --expt_name=finetune_val_SGD_0.1 --epochs=50 --lr=0.1 --lr_warmup_epochs=0 --seed=0 --alpha 0.984 0.992 0.996 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/checkpoint_last.pth.tar 
 python train.py --expt_name=finetune_val_SGD_0.3 --epochs=50 --lr=0.3 --lr_warmup_epochs=0 --seed=0 --alpha 0.984 0.992 0.996 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/checkpoint_last.pth.tar 
 python train.py --expt_name=finetune_val_SGD_0.03 --epochs=50 --lr=0.03 --lr_warmup_epochs=0 --seed=0 --alpha 0.984 0.992 0.996 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/checkpoint_last.pth.tar 
-python train.py --expt_name=finetune_val_SGD_0.01 --epochs=50 --lr=0.01 --lr_warmup_epochs=0 --seed=0 --alpha 0.984 0.992 0.996 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/checkpoint_last.pth.tar 
+python train.py --expt_name=finetune_val_SGD_0.8 --epochs=50 --lr=0.8 --lr_warmup_epochs=0 --seed=0 --alpha 0.984 0.992 0.996 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/checkpoint_last.pth.tar 
 wait
