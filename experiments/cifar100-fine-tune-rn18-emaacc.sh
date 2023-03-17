3db436cba68c88b545b3d7fda631507d3147f9ac
Morales97 <danimoralesbrotons@gmail.com>
2023-03-17 10:07:05 +0100

    .

---
+++ experiments/cifar100-fine-tune-rn18-emaacc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# train sequentially (no &)
+python train.py --expt_name=finetune_val_EMAacc_0.1 --epochs=50 --lr=0.1 --lr_warmup_epochs=0 --seed=0 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/best_ema_acc.pth.tar 
+python train.py --expt_name=finetune_val_EMAacc_0.3 --epochs=50 --lr=0.3 --lr_warmup_epochs=0 --seed=0 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/best_ema_acc.pth.tar 
+python train.py --expt_name=finetune_val_EMAacc_0.03 --epochs=50 --lr=0.03 --lr_warmup_epochs=0 --seed=0 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/best_ema_acc.pth.tar 
+python train.py --expt_name=finetune_val_EMAacc_0.01 --epochs=50 --lr=0.01 --lr_warmup_epochs=0 --seed=0 --dataset=cifar100 --net=rn18 --pretrained=/mloraw1/danmoral/checkpoints/tiny-in/rn18/val_0.8_s$1/best_ema_acc.pth.tar 
+wait
