a2d049083b0bc5314b2eb6637df3ffd02d15345f
Morales97 <danimoralesbrotons@gmail.com>
2023-04-21 16:53:35 +0200

    Update print_all_results.sh

--- print_all_results.sh
+++ print_all_results.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 python full_evaluation.py --net=rn18 --dataset=cifar100 --lr=0.8 --eval_on_test=False & 
 python full_evaluation.py --net=rn18 --dataset=cifar100 --lr=0.8 --eval_on_test=True &
+python full_evaluation.py --net=vgg16 --dataset=cifar100 --lr=0.06 --eval_on_test=False &
+python full_evaluation.py --net=vgg16 --dataset=cifar100 --lr=0.06 --eval_on_test=True &
+python full_evaluation.py --net=rn18 --dataset=cifar10 --lr=0.4 --eval_on_test=False &
+python full_evaluation.py --net=rn18 --dataset=cifar10 --lr=0.4 --eval_on_test=True &
 wait
